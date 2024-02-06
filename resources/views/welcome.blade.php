4c086f2ec3ea3253e03771fded5b0c943c1d9585
elisabichiappini <elisabichiappini@gmail.com>
2024-02-06 16:20:12 +0100

    feat: initial scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Laravel Boolfolio - Base</title>
     <!--inclusione assets con direttiva vite-->
     @vite('resources/js/app.js')
     <!--/inclusione assets con direttiva vite-->
 </head>
 <body>
-    <h1>Template</h1>
+    <h1>Laravel Boolfolio - Base</h1>
 </body>
 </html>
