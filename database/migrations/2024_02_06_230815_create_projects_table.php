2d7e9f0b12e972c81c235fd2af0ee13479544179
elisabichiappini <elisabichiappini@gmail.com>
2024-02-07 01:55:17 +0100

    feat: seeder with faker

--- database/migrations/2024_02_06_230815_create_projects_table.php
+++ database/migrations/2024_02_06_230815_create_projects_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            //titoli colonna tabella portofolio di progetti
+            $table->string('title', 50)->unique();
+            $table->string('slug', 200);
+            $table->string('tools', 100)->nullable();
+            $table->string('type', 100)->nullable();
+            $table->text('description')->nullable();
+            $table->date('born')->nullable();
             $table->timestamps();
         });
     }
