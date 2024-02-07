35f9918264f8923de54ff7015844cc8de95e0ceb
elisabichiappini <elisabichiappini@gmail.com>
2024-02-07 21:54:42 +0100

    feat: order in folders

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,7 +14,7 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        {{ __('Benvenuto in Area Admin, accesso consentito.') }}
                     </div>
                 </div>
             </div>
